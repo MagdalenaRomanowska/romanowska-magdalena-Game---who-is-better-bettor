1fd61b5867b20f0ba5dce712bdcebf4f1ec71931
Magdalena Romanowska <magdalena.romanowska.poczta@gmail.com>
2021-02-25 11:48:01 +0100

    styled view

--- src/components/zad10 - Zaklad_kto_lepiej_obstawia/Zaklad_kto_lepiej_obstawia.js
+++ src/components/zad10 - Zaklad_kto_lepiej_obstawia/Zaklad_kto_lepiej_obstawia.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styles from './Zaklad_kto_lepiej_obstawia.scss';
-import Button from '@material-ui/core/Button';
 
 class Zaklad_kto_lepiej_obstawia extends React.Component {
   constructor(props) {
@@ -139,139 +138,138 @@ class Zaklad_kto_lepiej_obstawia extends React.Component {
       <div className={styles.main}>
         <div className={styles.question}>
           <div className={styles.line}></div>
-          <div className={styles.title}>And now a small game for two:</div>
-          <div className={styles.titleGame}>Who is better bettor? :)</div>
+          <div className={styles.title}>
+            <i className={'fas fa-user-friends'}></i> Game for two players
+          </div>
+          <div className={styles.titleGame}>
+            Wh<i className={'fas fa-dice-d6'}></i> is better bettor?{' '}
+          </div>
           <div className={styles.gameExplanation}>
             This game is about guessing the average amount of dots on 3 dices
-            after throwing them as many times as the players want.<br></br>
-            Players put their names and the subjects to bet on. They also put
-            the numbers of walls (so also the dots) that dices have and how many
-            times they will be thrown. After that the players bet on the result
-            - the smallest number they can bet is of course 3 (3 dices, and on
-            each result-wall with 1 dot). Maximum number to bet is the sum of
-            all the walls (=dots) of these 3 dices. The winner is the one whose
-            guess is closer to the result of throwing the dices.
+            after throwing them as many times as the players want. Players put
+            their names and the subjects to bet on. After that the players bet
+            on the result - the smallest number they can bet is of course 3 (3
+            dices, and on each result-wall with 1 dot). Maximum number to bet is
+            the sum of all the walls (and also the dots) of these 3 dices. The
+            players also put the numbers of walls (so also the dots) that dices
+            have and how many times they will be thrown. The winner is the one
+            whose guess is closer to the result of throwing the dices.
           </div>
           <div className={styles.users}>
-            <div className={styles.userName1}>User 1 - what is your name?</div>
-            <input
-              placeholder='User 1 - write your name'
-              className={styles.inputName}
-              onChange={(event) =>
-                this.setState({
-                  firstUserName: event.target.value,
-                })
-              }
-            ></input>
-            <div className={styles.userName2}>User 2 - what is your name?</div>
-            <input
-              placeholder='User 2 - write your name'
-              className={styles.inputName}
-              onChange={(event) =>
-                this.setState({
-                  secondUserName: event.target.value,
-                })
-              }
-            ></input>
-            <div className={styles.userName2}>
-              {this.state.firstUserName} - what do you bet about?
+            <div className={styles.questionsInline}>
+              <input
+                placeholder='User 1 - write your name'
+                className={styles.inputName}
+                onChange={(event) =>
+                  this.setState({
+                    firstUserName: event.target.value,
+                  })
+                }
+              ></input>
+
+              <input
+                placeholder='User 2 - write your name'
+                className={styles.inputName}
+                onChange={(event) =>
+                  this.setState({
+                    secondUserName: event.target.value,
+                  })
+                }
+              ></input>
             </div>
-            <input
-              placeholder='User 1 - what do you bet about?'
-              className={styles.inputName}
-              onChange={(event) =>
-                this.setState({
-                  betUser1: event.target.value,
-                })
-              }
-            ></input>
-            <div className={styles.userName2}>
-              {this.state.secondUserName} - what do you bet about?
+            <div className={styles.questionsInline}>
+              <input
+                placeholder='User 1 - what do you bet about?'
+                className={styles.inputName}
+                onChange={(event) =>
+                  this.setState({
+                    betUser1: event.target.value,
+                  })
+                }
+              ></input>
+
+              <input
+                placeholder='User 2 - what do you bet about?'
+                className={styles.inputName}
+                onChange={(event) =>
+                  this.setState({
+                    betUser2: event.target.value,
+                  })
+                }
+              ></input>
             </div>
+            <div className={styles.questionsInline}>
+              <input
+                placeholder='User 1 - what result do you bet?'
+                className={styles.inputName}
+                onChange={(event) =>
+                  this.setState({
+                    firstUserBetsOnTheResult: firstUserBetsOnTheResult(
+                      event.target.value
+                    ),
+                  })
+                }
+              ></input>
+
+              <input
+                placeholder='User 2 - what result do you bet?'
+                className={styles.inputName}
+                onChange={(event) =>
+                  this.setState({
+                    secondUserBetsOnTheResult: secondUserBetsOnTheResult(
+                      event.target.value
+                    ),
+                  })
+                }
+              ></input>
+            </div>
+
+            <div className={styles.userData}>How many walls do dices have?</div>
+            <div className={styles.numberOfWallsInputs}>
+              <input
+                placeholder='first dice'
+                className={styles.numberOfWallsInput}
+                onChange={(event) =>
+                  this.setState({
+                    firstDice: firstDice(event.target.value),
+                  })
+                }
+              ></input>
+              <input
+                placeholder='second dice'
+                className={styles.numberOfWallsInput}
+                onChange={(event) =>
+                  this.setState({
+                    secondDice: secondDice(event.target.value),
+                  })
+                }
+              ></input>
+              <input
+                placeholder='third dice'
+                className={styles.numberOfWallsInput}
+                onChange={(event) =>
+                  this.setState({
+                    thirdDice: thirdDice(event.target.value),
+                  })
+                }
+              ></input>
+            </div>
+          </div>
+          <div className={styles.userData}>Write a number of dices throws</div>
+          <div className={styles.throwsAmount}>
             <input
-              placeholder='User 2 - what do you bet about?'
-              className={styles.inputName}
+              placeholder='throws amount'
+              className={styles.throwsAmountInput}
               onChange={(event) =>
                 this.setState({
-                  betUser2: event.target.value,
-                })
-              }
-            ></input>
-            <div className={styles.user1}>How many walls do dices have?</div>
-            <input
-              placeholder='User 1 - write a number of walls of the first dice'
-              className={styles.input}
-              onChange={(event) =>
-                this.setState({
-                  firstDice: firstDice(event.target.value),
-                })
-              }
-            ></input>
-            <input
-              placeholder='User 1 - write a number of walls of the second dice'
-              className={styles.input}
-              onChange={(event) =>
-                this.setState({
-                  secondDice: secondDice(event.target.value),
-                })
-              }
-            ></input>
-            <input
-              placeholder='User 1 - write a number of walls of the third dice'
-              className={styles.input}
-              onChange={(event) =>
-                this.setState({
-                  thirdDice: thirdDice(event.target.value),
+                  howManyDiceThrows: howManyDiceThrows(event.target.value),
                 })
               }
             ></input>
           </div>
-          <div className={styles.user}>
-            {this.state.firstUserName} and {this.state.secondUserName} - write a
-            number of dices throws
-          </div>
-          <input
-            placeholder='both users - write a number of dices throws'
-            className={styles.input}
-            onChange={(event) =>
-              this.setState({
-                howManyDiceThrows: howManyDiceThrows(event.target.value),
-              })
-            }
-          ></input>
-          <div className={styles.user}>
-            {this.state.firstUserName} bets on the result:
-          </div>
-          <input
-            placeholder='User 1 - what result do you bet?'
-            className={styles.input}
-            onChange={(event) =>
-              this.setState({
-                firstUserBetsOnTheResult: firstUserBetsOnTheResult(
-                  event.target.value
-                ),
-              })
-            }
-          ></input>
-          <div className={styles.user}>
-            {this.state.secondUserName} bets on the result:
-          </div>
-          <input
-            placeholder='User 2 - what result do you bet?'
-            className={styles.input}
-            onChange={(event) =>
-              this.setState({
-                secondUserBetsOnTheResult: secondUserBetsOnTheResult(
-                  event.target.value
-                ),
-              })
-            }
-          ></input>
           <div className={styles.result}>
-            <Button
+            <button
               className={styles.buttonToClick}
-              variant='outlined'
-              color='primary'
               onClick={() =>
                 this.setState({
                   result: whoWinsTheBet(
@@ -286,19 +284,26 @@ class Zaklad_kto_lepiej_obstawia extends React.Component {
                     this.state.betUser1,
                     this.state.betUser2
                   ),
-                })       
+                })
               }
             >
-              Throw the dices
-            </Button>
-            <div className={styles.resultText}>{this.state.result}</div>
+              <div className={styles.buttonToClickText}>
+                <i className={'fas fa-dice-four'}></i>
+                <i className={'fas fa-dice-d20'}></i>
+                <i className={'fas fa-dice-five'}></i> Throw the dices
+                <i className={'fas fa-dice-six'}></i>
+                <i className={'fas fa-cube'}></i>
+                <i className={'fas fa-dice-two'}></i>
+              </div>
+            </button>
           </div>
-          
+          <div className={styles.resultText}>{this.state.result}</div>
         </div>
-        <div className={styles.line}></div>
       </div>
     );
   }
 }
 
 export default Zaklad_kto_lepiej_obstawia;
+
+//position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%)
